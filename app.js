7d725a6a08c8a962afc9ed8a4e2135c915fbef46
Rouan Wilsenach <rouanw@gmail.com>
2013-12-03 14:57:29 +0200

    example add item to collection via post

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/userlist', routes.userlist(db));
+app.get('/newuser', routes.newuser);
+
+app.post('/adduser', routes.adduser(db));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
